28859a94b183a9deb454cf474ca92a0637db6443
vivek singh <154013086+vivek-singh226@users.noreply.github.com>
2024-10-12 00:02:45 +0530

    + controller

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,12 +1,15 @@
-const {signup} = require('../Controllers/AuthController')
-const{signupValidation} = require ('../Middlewares/AuthValidation');
+const {signup, login} = require('../Controllers/AuthController')
+const{signupValidation, loginValidation} = require ('../Middlewares/AuthValidation');
 
 const router = require('express').Router();
+
 
 router.post('/login',(req,res)=>{
     res.send('login success');
 });
+// 
 
+router.post('/login', loginValidation, login);
 router.post('/signup', signupValidation, signup);
 
 module.exports = router;
