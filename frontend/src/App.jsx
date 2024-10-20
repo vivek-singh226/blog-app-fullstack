2a062ea83383ab55903936e5146a7ddc3264014c
vivek singh <154013086+vivek-singh226@users.noreply.github.com>
2024-10-20 19:42:27 +0530

    latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route , Routes } from 'react-router-dom';
+import { Navigate ,Route , Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Login';
 import Signup from './Signup';
--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify';
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className='container'>
+      <h1>SIGNUP</h1>
+      <form>
+        <div>
+          <label htmlFor='name'>Name </label>
+          <input 
+          type='text'
+          name='name'
+          autoFocus
+          placeholder='Enter your name'
+          />
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input 
+          type='email'
+          name='email'
+          placeholder='Enter your email'
+          />
+        </div>
+        <div>
+          <label htmlFor='password'> Password </label>
+          <input 
+          type='password'
+          name='password'
+          placeholder='Enter your password'
+          />
+        </div>
+        <button>Signup</button>
+        <span> Already have an account ? 
+          <Link to="/login">Login</Link>
+        </span>
+      </form>
+<ToastContainer />
+    </div>
   )
 }
 
